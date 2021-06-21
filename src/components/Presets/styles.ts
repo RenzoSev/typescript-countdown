f3b352ebbbd85f429bb34aaad1b5fa722fb84c05
Renzo Sevilha <sevilharenzo@gmail.com>
2021-06-21 11:45:30 -0300

    feat: styles xl screen buttons

--- src/components/Presets/styles.ts
+++ src/components/Presets/styles.ts
@@ -1,41 +1,53 @@
 import styled from 'styled-components';
 
 const DivPresets = styled.div`
-    display: flex;
-    flex-flow: row wrap;
-    justify-content: space-around;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-around;
 
-    width: 50%;
+  width: 50%;
+
+  & > button {
+    flex-basis: 40%;
+
+    font-size: 1.2rem;
+
+    border: none;
+    border-radius: 5px;
+    box-shadow: var(--shadow);
+
+    margin: 0.4rem;
+    padding: 0.8rem;
+
+    &:disabled {
+      opacity: 40%;
+    }
+
+    transition: all 0.5s;
+  }
+
+  @media (min-width: 768px) {
+    & {
+      width: 30%;
+    }
 
     & > button {
-        flex-basis: 40%;
+      flex-basis: 35%;
+    }
+  }
 
-        font-size: 1.2rem;
-
-        border: none;
-        border-radius: 5px;
-        box-shadow: var(--shadow);
-
-        margin: 0.4rem;
-        padding: 0.8rem;
-
-        &:disabled {
-            opacity: 40%;
-        }
-
-        transition: all 0.5s;
+  @media (min-width: 1800px) {
+    & {
+      width: 30%;
     }
 
-
-    @media(min-width: 768px) {
-        & {
-            width: 30%;
-        }
-        
-        & > button {
-            flex-basis: 35%;
-        }
+    & > button {
+      flex-basis: 35%;
+      font-size: 1.4rem;
+      padding: 0.95rem;
+      letter-spacing: 0.8px;
     }
+  }
 `;
 
 export default DivPresets;
